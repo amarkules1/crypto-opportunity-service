e2980b42aced29a000daf3d59991600ad70b76bc
amarkules1 <amarlar2354@gmail.com>
2023-07-13 23:18:49 -0400

    get something doing something

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sqlalchemy import sql
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_cors import CORS
+import requests
 
 
 app = Flask(__name__, static_folder='crypto-opportunity-front-end/dist', static_url_path='')
@@ -25,6 +26,23 @@ secret_sauce = json.load(open('secret_sauce.json',))
 @app.route('/')
 def status():
     return "ok"
+
+
+@app.route("/price-hist", methods=['GET'])
+def price_hist():
+    return fetch_daily_data("BTC/USD").to_json(orient="records")
+
+
+def fetch_daily_data(symbol):
+    pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/EUR
+    symbol = pair_split[0] + '-' + pair_split[1]
+    url = f'https://api.pro.coinbase.com/products/{symbol}/candles?granularity=86400'
+    response = requests.get(url)
+    if response.status_code == 200:  # check to make sure the response from server is good
+        data = pd.DataFrame(json.loads(response.text), columns=['unix', 'low', 'high', 'open', 'close', 'volume'])
+        data['date'] = pd.to_datetime(data['unix'], unit='s')  # convert to a readable date
+        data['vol_fiat'] = data['volume'] * data['close']
+    return data
 
 
 if __name__ == '__main__':
